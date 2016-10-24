3c3bc62c2fc897edbbf1f2c86e7335977b3bdd91
Juan Olalla <juan.olalla@gmail.com>
2016-10-24 11:00:16 +0200

    Check that  has been set in the nav template

--- app/templates/nav.php
+++ app/templates/nav.php
@@ -1,13 +1,13 @@
 <div id="nav" class="clearfix">
 
   <ul class="menu list-tabs-informe">
-    <li class="menu-item menu-item-1<?php print ($active_menu == '1') ? ' active' : ''; ?>">
+    <li class="menu-item menu-item-1<?php print (isset($active_menu) && $active_menu == '1') ? ' active' : ''; ?>">
       <a href="<?php print BASE_PATH; ?>catalog">Catalog</a></li>
-    <li class="menu-item menu-item-2<?php print ($active_menu == '2') ? ' active' : ''; ?>">
+    <li class="menu-item menu-item-2<?php print (isset($active_menu) && $active_menu == '2') ? ' active' : ''; ?>">
       <a href="<?php print BASE_PATH; ?>services">Services</a></li>
-    <li class="menu-item menu-item-3<?php print ($active_menu == '3') ? ' active' : ''; ?>">
+    <li class="menu-item menu-item-3<?php print (isset($active_menu) && $active_menu == '3') ? ' active' : ''; ?>">
       <a href="<?php print BASE_PATH; ?>about">About</a></li>
-    <li class="menu-item menu-item-4<?php print ($active_menu == '4') ? ' active' : ''; ?>">
+    <li class="menu-item menu-item-4<?php print (isset($active_menu) && $active_menu == '4') ? ' active' : ''; ?>">
       <a href="<?php print BASE_PATH; ?>contact">Contact</a></li>
   </ul>
 
